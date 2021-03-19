0e8c7b6ac347ea8f076380087d398d21b3827b80
pawarss1 <pawarss_1@rknec.edu>
2021-03-19 18:17:30 +0530

    Added design pattern

--- Tests/test.js
+++ Tests/test.js
@@ -8,7 +8,7 @@ describe("Unit Testing module for Player Module", () => {
     "Unit testing for Point variable getter and setter methods",
     function () {
       it("should test getter and setter methods for points instance variable", (done) => {
-        const Player1 = new Player(1);
+        const Player1 = new Player([1]);
         Player1.updatePointsBy(2);
         expect(2).to.equal(Player1.getPoints());
         done();
@@ -19,7 +19,7 @@ describe("Unit Testing module for Player Module", () => {
     "Unit testing for successiveMisses variable getter and setter methods",
     function () {
       it("should test getter and setter methods for successiveMisses instance variable", (done) => {
-        const Player1 = new Player(1);
+        const Player1 = new Player([1]);
         Player1.updateSuccessiveMissesBy(2);
         expect(2).to.equal(Player1.getSuccessiveMisses());
         done();
@@ -30,7 +30,7 @@ describe("Unit Testing module for Player Module", () => {
     "Unit testing for foulCounts variable getter and setter methods",
     function () {
       it("should test getter and setter methods for foulCounts instance variable", (done) => {
-        const Player1 = new Player(1);
+        const Player1 = new Player([1]);
         Player1.updateFoulPointsBy(2);
         expect(2).to.equal(Player1.getFoulPoints());
         done();
@@ -42,9 +42,9 @@ describe("Unit Testing module for Player Module", () => {
 describe("Unit Testing module for Board Module", () => {
   context("Unit testing for Board class methods", function () {
     it("should test all the 6 outcomes", (done) => {
-      const newBoard = new Board(9, 5);
-      const PlayerTemp1 = new Player(1);
-      const PlayerTemp2 = new Player(2);
+      const newBoard = new Board([9, 5]);
+      const PlayerTemp1 = new Player([1]);
+      const PlayerTemp2 = new Player([2]);
       newBoard.strike(PlayerTemp1);
       newBoard.multiStrike(PlayerTemp1);
       newBoard.redStrike(PlayerTemp1);
@@ -60,9 +60,9 @@ describe("Unit Testing module for Board Module", () => {
   });
   context("Unit testing for Board class methods", function () {
     it("should test for draw condition", (done) => {
-      const newBoard = new Board(1, 1);
-      const PlayerTemp1 = new Player(1);
-      const PlayerTemp2 = new Player(2);
+      const newBoard = new Board([1, 1]);
+      const PlayerTemp1 = new Player([1]);
+      const PlayerTemp2 = new Player([2]);
       newBoard.simulate(1, PlayerTemp1, PlayerTemp2);
       const response = newBoard.simulate(3, PlayerTemp2, PlayerTemp1);
       expect(response.draw).to.equal(true);
@@ -71,9 +71,9 @@ describe("Unit Testing module for Board Module", () => {
   });
   context("Unit testing for Board class methods", function () {
     it("should test for Player1 win condition", (done) => {
-      const newBoard = new Board(3, 1);
-      const PlayerTemp1 = new Player(1);
-      const PlayerTemp2 = new Player(2);
+      const newBoard = new Board([3, 1]);
+      const PlayerTemp1 = new Player([1]);
+      const PlayerTemp2 = new Player([2]);
       newBoard.simulate(1, PlayerTemp1, PlayerTemp2);
       newBoard.simulate(4, PlayerTemp2, PlayerTemp1);
       newBoard.simulate(3, PlayerTemp1, PlayerTemp2);
---
+++ Util.js
@@ -0,0 +1,78 @@
+const CreateInstancesFactory = require("./CreateInstancesFactory");
+const DataPreProcessing = require("./DataPreProcessing");
+
+//Utility and Initialization methods encapsulated inside Utils Class.
+class Util {
+  static startGame() {
+    try {
+      //Static class to get the structured input from file.
+      const reponseObj = DataPreProcessing.getInputFromFile("InputFile.txt");
+      if (!reponseObj.success) {
+        console.log(
+          "Error while parsing the input file, maybe due to improper input format, Please check Readme file for better understanding."
+        );
+      } else {
+        //Board constructor takes in two parameters, number of black coins and number of red coins
+        const newBoard = CreateInstancesFactory.createInstance("Board", [
+          reponseObj.coins["Black"],
+          reponseObj.coins["Red"],
+        ]);
+
+        /*
+          Player constructor takes in one parameter, Player Serial Number or Id. 
+          This should be same as # mentioned in the input file 
+        */
+
+        //This factory returns the instance of the Player Class; with ID as parameter.
+        const Player1 = CreateInstancesFactory.createInstance("Player", [1]);
+        const Player2 = CreateInstancesFactory.createInstance("Player", [2]);
+
+        const inputArr = reponseObj.inputArr;
+        let winner = null;
+        let response = { draw: false, win: false };
+        console.log(`Intermediate Scorings: `);
+
+        for (let i = 0; i < inputArr.length; i++) {
+          //Method to handle all the conditions, constraints and simulate the process.
+          response = newBoard.simulate(
+            inputArr[i][1],
+            inputArr[i][0] === 1 ? Player1 : Player2,
+            inputArr[i][0] === 1 ? Player2 : Player1
+          );
+          if (response.draw || response.win) {
+            winner = inputArr[i][0];
+            break;
+          }
+          console.log(
+            `Player1: ${Player1.getPoints()}  Player2: ${Player2.getPoints()}`
+          );
+        }
+        if (response.draw) {
+          console.log(
+            `Match Drawn. Final Score- ${Player1.getPoints()}:${Player2.getPoints()}`
+          );
+        } else if (response.win) {
+          console.log(
+            `Player${winner} won the game. Final Score- ${
+              winner === 1 ? Player1.getPoints() : Player2.getPoints()
+            }:${winner === 1 ? Player2.getPoints() : Player1.getPoints()}`
+          );
+        } else {
+          /*
+          "When the coins are exhausted on the board, if the highest scorer is not leading by, at
+          least, 3 points or does not have a minimum of 5 points, the game is considered a draw"
+          As in the condition, it is mentioned only when the coins are exhausted, then can a match be drawn,
+          but lets say we have set of input where in the coins are not exhausted, or the input is incomplete
+          in that case, we cant come to a conclusion.
+        */
+          console.log(
+            `Couldn't reach to any conclusion with the given input..(Can be considered as Match drawn, but kept a seperate case for more clarity). Final Score: Player1: ${Player1.getPoints()} : Player2: ${Player2.getPoints()}`
+          );
+        }
+      }
+    } catch (err) {
+      console.log("Technical Issue or exception " + err);
+    }
+  }
+}
+module.exports = Util;
